2a62473f13088d52b312842c75d77f1b8cfc416c
heisson <heisson70@gmail.com>
2020-05-25 19:06:21 -0300

    Testing gitignore

---
+++ spreadsheet/test2.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+spreadsheet_id = "11sCfPdirODppDGEa5bqAsUW6gO6A6oY4BXUpYlXtQOg"
+
+
+def main():
+	credentials = None
+
+	if os.
+
+
+if __name__ == '__main__':
+	main()
